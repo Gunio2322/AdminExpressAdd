c25d7119b84dcd9eafb02f8dbf6e2f5a6ae74c93
gunio2322 <gunio2322@gmail.com>
2023-06-08 16:19:18 +0200

    start

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const handlebarsExpress = require('express-handlebars');
+const bodyParser = require('body-parser');
+const hbs = handlebarsExpress.create({
+    extname: '.hbs',
+    partialsDir: __dirname + './../views/partials/'
+});
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
+app.set('views', __dirname + './../views');
+app.use(express.static(__dirname + './../public'));
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+
+app.get('/kontakt', (req, res) => {
+    res.render('kontakt')
+})
+
+
+app.listen(3000, ()=> {console.log('server start 3000')})
